0b99b9fae557d530b2a8b22691b9391b451cba8e
Adrian <ajurczenia4219@stu.d214.org>
2023-08-24 11:00:48 -0500

    Initial Commit

---
+++ ContentView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image("Unknown-1")
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Penaldo sucks!")
+        }
+    }
+}
